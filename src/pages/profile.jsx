a6f0f6ff5bce1e3e35dd01e795dd454187ec01c0
Chris Clarke <chrisclarke1977@gmail.com>
2022-08-10 17:55:48 +0100

    coming soon message on profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -9,6 +9,17 @@ export default function Profile() {
             <main className="site-main">
                 <div className="container">
                     <h1>Kez and Unicorns Profile</h1>
+                    <section>
+                        <p>
+                            Coming soon ...{" "}
+                            <span role="img" aria-label="heart">
+                                💖
+                            </span>
+                            <span role="img" aria-label="rainbow">
+                                🌈
+                            </span>
+                        </p>
+                    </section>
                 </div>
             </main>
             <Bottom />
